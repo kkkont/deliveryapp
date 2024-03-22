cf7a8797b969f3b207326ad962ec30619bb8f371
kkkont <116272343+kkkont@users.noreply.github.com>
2024-03-22 17:50:15 +0200

    File-based persistence

--- src/main/java/com/example/deliveryapp/entity/WeatherData.java
+++ src/main/java/com/example/deliveryapp/entity/WeatherData.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
-
+@Table(name = "WEATHER_DATA")
 public class WeatherData {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
